e3c9d86fa64f52fc1cfee930e2104318baf4b1ed
yubo <yubo1911@163.com>
2016-04-14 01:16:34 -0700

    2nd version decorator. Add the kwargs and handle the unhashable issue.

--- decorator/decorator_example.py
+++ decorator/decorator_example.py
@@ -2,27 +2,33 @@
 
 def func_cache(func):
 	cache = {}
-	def inner_deco(*args):
-		if args in cache:
-			print('func {} is already cached with arguments {}'.format(
-				func.__name__, args))
-			return cache[args]
-		else:
-			print('func {} is not cached with arguments {}'.format(
-				func.__name__, args)) 
-			res = func(*args)
-			cache[args] = res
-			return res
+	def inner_deco(*args, **kwargs):
+		key = (args, frozenset(kwargs.items()))
+		if key not in cache:
+			print('func {} is not cached with arguments {} {}'.format(
+				func.__name__, args, kwargs)) 
+			res = func(*args, **kwargs)
+			cache[key] = res
+		return cache[key]
 	return inner_deco
 
 @func_cache
 def add_two_number(a, b):
 	return a + b
 
+@func_cache
+def product_two_number(a, b):
+	return a * b
+
 if __name__ == "__main__":
 	print('1. add_two_number(1, 2)')
 	add_two_number(1, 2)
+	print(add_two_number.__name__)
 	print('2. add_two_number(2, 3)')
 	add_two_number(2, 3)
-	print('3. add_two_number(1, 2)')
+	print('3. add_two_number(1, b=2)')
+	add_two_number(1, b=2)
+	print('4. add_two_number(1, 2)')
 	add_two_number(1, 2)
+	print('5. product_two_number(1, 2)')
+	product_two_number(1, 2)
