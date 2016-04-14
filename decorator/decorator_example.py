60a87299869045e0b93cf3d628c808c841985687
yubo <yubo1911@163.com>
2016-04-14 01:27:31 -0700

    4th version decorator. Add parameter to decorator.

--- decorator/decorator_example.py
+++ decorator/decorator_example.py
@@ -1,24 +1,32 @@
 # -*- coding: utf-8 -*-
 from functools import wraps
+import random
 
-def func_cache(func):
-	cache = {}
-	@wraps(func)
-	def inner_deco(*args, **kwargs):
-		key = (args, frozenset(kwargs.items()))
-		if key not in cache:
-			print('func {} is not cached with arguments {} {}'.format(
-				func.__name__, args, kwargs)) 
-			res = func(*args, **kwargs)
-			cache[key] = res
-		return cache[key]
-	return inner_deco
+def outer_deco(size=10):
+	def func_cache(func):
+		cache = {}
+		@wraps(func)
+		def inner_deco(*args, **kwargs):
+			key = (args, frozenset(kwargs.items()))
+			if key not in cache:
+				print('func {} is not cached with arguments {} {}'.format(
+					func.__name__, args, kwargs)) 
+				res = func(*args, **kwargs)
+				if len(cache) >= size:
+					lucky_key = random.choice(list(cache.keys()))
+					print('func {} cache pop {}'.format(
+						func.__name__, lucky_key))
+					cache.pop(lucky_key, None)
+				cache[key] = res
+			return cache[key]
+		return inner_deco
+	return func_cache
 
-@func_cache
+@outer_deco(size=3)
 def add_two_number(a, b):
 	return a + b
 
-@func_cache
+@outer_deco()
 def product_two_number(a, b):
 	return a * b
 
@@ -26,7 +34,6 @@ if __name__ == "__main__":
 	print('add_two_number func name is {}'.format(add_two_number.__name__))
 	print('1. add_two_number(1, 2)')
 	add_two_number(1, 2)
-	print(add_two_number.__name__)
 	print('2. add_two_number(2, 3)')
 	add_two_number(2, 3)
 	print('3. add_two_number(1, b=2)')
@@ -35,3 +42,5 @@ if __name__ == "__main__":
 	add_two_number(1, 2)
 	print('5. product_two_number(1, 2)')
 	product_two_number(1, 2)
+	print('6. add_two_number(1, 3)')
+	add_two_number(1, 3)
