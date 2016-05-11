32eebd4c95eed890ce148c832fcdd710efbd081d
yubo <yubo1911@163.com>
2016-05-10 20:34:42 -0700

    LCTHW try some formats.

--- learncthehardway/ex6.c
+++ learncthehardway/ex6.c
@@ -9,12 +9,12 @@ int main(int argc, char *argv[])
 	char first_name[] = "Zed";
 	char last_name[] = "Shaw";
 
-	printf("You are %d miles away.\n", distance);
-	printf("You have %f levels of power.\n", power);
-	printf("You have %f awesome super powers.\n", super_power);
-	printf("I have an initial %c.\n", initial);
-	printf("I have a first name %s.\n", first_name);
-	printf("I have a last name %s.\n", last_name);
+	printf("You are %.5d miles away.\n", distance);
+	printf("You have %.6f levels of power.\n", power);
+	printf("You have %.3f awesome super powers.\n", super_power);
+	printf("I have an initial %10c.\n", initial);
+	printf("I have a first name %10s.\n", first_name);
+	printf("I have a last name %10s.\n", last_name);
 	printf("My whole name is %s.%c. %s.\n", first_name, initial, last_name);
 
 	return 0;
