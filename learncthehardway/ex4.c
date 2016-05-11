679ff7d20fc4ddb59358ca72736e707b1c6bdc3a
yubo <yubo1911@163.com>
2016-05-10 19:52:07 -0700

    LCTHW ex4. The warning version of ex3.

---
+++ learncthehardway/ex4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int age = 10;
+	int height;
+	printf("I am %d years old.\n");
+	printf("I am %d inches tall.\n", height);
+
+	return 0;
+}
