0f3ec27f5fbfa76220f42aec6e1bfbafd5a12fea
yubo <yubo1911@163.com>
2016-05-16 01:06:24 -0700

    LCTHW ex10

---
+++ learncthehardway/ex10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	for (i=1; i<argc; ++i)
+	{
+		printf("arg %d:%s\n", i, argv[i]);
+	}
+
+	char *states[] = {
+		"California", "Oregon",
+		"Washington", "Texas"
+	};
+
+	int num_states = 4;
+	for (i=0; i<num_states; ++i)
+	{
+		printf("state %d:%s\n", i, states[i]);
+	}
+
+	return 0;
+}
