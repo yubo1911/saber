1247bc599d00f98d63b9577bbd8326c8659c7334
Yubo home <yubo1911@163.com>
2016-05-27 22:16:51 -0700

    LCTHW ex28. Add tests.sh

---
+++ learncthehardway/c-skeleton/tests/runtests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Running unit test:"
+
+for i in tests/*_tests
+do
+	if test -f $i
+	then
+		if $VALGRIND ./$i 2>> tests/tests.log
+		then
+			echo $i PASS
+		else
+			echo "ERROR in test $i: here's tests/tests.log"
+			echo "---"
+			tail tests/tests.log
+			exit 1
+		fi
+	fi
+done
+
+echo ""
