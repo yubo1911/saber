d6cb83d3c02ce7aa89523af1358c02425ced277c
Yubo home <yubo1911@163.com>
2016-06-21 17:23:36 -0700

    learncpp11 auto

---
+++ learncpp11/auto_decltype.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	// simple auto
+	double val1 = 1.1, val2 = 2.2;
+	auto sum = val1 + val2;
+	auto val3 = 0.3, *p = &val3;
+	//auto val4 = 0, val5 = 0.0; // wrong. different types.
+	//auto sum2; // wrong, auto type must be initialized.
+	double val6 = 1.6, &rval6 = val6;
+	auto aval6 = rval6;
+	aval6 = 6.0; // aval6 is not a reference.
+
+	cout<<"test simple auto:\n"<<val1<<'\t'<<val2<<'\t'<<sum<<'\t'<<val3<<'\t'<<p<<'\t'<<val6<<'\t'<<rval6<<'\t'<<aval6<<endl;
+
+	// const and auto
+	const int val7 = 1, &rval7 = val7;
+	auto aval7 = val7; // remove top-level const
+	aval7 = 7;
+	auto aval8 = rval7; // remove top-level const
+	aval8 = 8;
+	auto aval9 = &val6; // not const
+	*aval9 = 9;
+	auto aval10 = &val7; // keep low-level const
+	//*aval10 = 10; // wrong. const int can't be changed.
+	const auto aval11 = val7; // top-level const auto
+	//aval11 = 11; // wrong. const int can't be changed.
+	auto &aval12 = val7; // keep top-level const
+	//aval12 = 12; // wrong. const reference
+	auto &aval13 = val6;
+	aval13 = 13.0;
+	//auto &aval14 = 42; // wrong. must be const auto
+	const auto &aval15 = 15;
+	//aval15 = 16; // wrong. const reference.
+	//auto &aval16 = aval7, *aval17 = &val7; // wrong. type not consistent
+	cout<<"test auto and const:\n"<<val7<<'\t'<<rval7<<'\t'<<aval7<<'\t'<<aval8<<'\t'<<*aval9<<'\t'<<aval10<<'\t'<<aval11<<'\t'<<aval12<<'\t'<<aval13<<'\t'<<aval15<<endl;
+}
