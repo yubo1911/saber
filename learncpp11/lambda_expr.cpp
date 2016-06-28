81324e365dbd8e793f8b731d8d48a67597b73bf1
Yubo home <yubo1911@163.com>
2016-06-27 17:50:11 -0700

    learncpp11 lambda part 1.

---
+++ learncpp11/lambda_expr.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	// lambda: [capture list] (parameter list) -> return type { function body }
+	cout<<"Test simple lambda f1:\n";
+	auto f1 = [] { return 1; };
+	auto f2 = [] () { return 2; };
+	cout<<f1()<<'\t'<<f2()<<endl;
+
+	cout<<"Test lambda with capture and parameter:\n";
+	int test_data[] = {1, 5, 9, 7, 3, 19, 13, 17};
+	int border = 8;
+	auto f3 = [border](const int &i){ if(i > border) cout<<i<<'\t'; };
+	for_each(begin(test_data), end(test_data), f3);
+	cout<<endl;
+
+	cout<<"Test lambda with capture reference:\n";
+	auto f4 = [&border](const int &i){ if(i > border) cout<<i<<'\t'; };
+	border = 6;
+	for_each(begin(test_data), end(test_data), f4);
+	cout<<endl;
+	
+	cout<<"Test lambda with implicit capture:\n";
+	char space = ' ';
+	auto f5 = [=](const int &i){ if(i > border) cout<<i<<'\t'; };
+	auto f6 = [&](const int &i){ if(i > border) cout<<i<<'\t'; };
+	auto f7 = [&, space](const int &i){ if(i > border) cout<<i<<space; };
+	border = 0;
+	for_each(begin(test_data), end(test_data), f5);
+	cout<<endl;
+	for_each(begin(test_data), end(test_data), f6);
+	cout<<endl;
+	for_each(begin(test_data), end(test_data), f7);
+	cout<<endl;
+
+	cout<<"Test lambda with implicit capture:\n";
+	auto f8 = [&, space](const int &i) mutable { if(i > border) {cout<<i<<space; space='\t';} };
+	for_each(begin(test_data), end(test_data), f8);
+	cout<<endl;
+	cout<<1<<space<<2<<endl;
+}
