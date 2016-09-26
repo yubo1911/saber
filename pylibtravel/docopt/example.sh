888e2ab7812fe871cd5cf00e181829e257e3b628
Yubo home <yubo1911@163.com>
2016-09-26 07:55:29 -0700

    py-grep. fix a bug about abspath and inner path

--- pylibtravel/docopt/example.sh
+++ pylibtravel/docopt/example.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+echo './py-grep . "(\w)+@(\w)+((\.\w+)+)" --ignorepath ./test_path1' 
 ./py-grep . "(\w)+@(\w)+((\.\w+)+)" --ignorepath ./test_path1 
+echo '===================='
+echo './py-grep . "(\w)+@(\w)+((\.\w+)+)" --ignorepath test_path1'
+./py-grep . "(\w)+@(\w)+((\.\w+)+)" --ignorepath test_path1 
