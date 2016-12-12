8c144c719ee628ae9082e72425acc40a73742d49
yubo <yubo1911@163.com>
2016-12-11 22:52:02 -0800

    figure out how to refresh python

--- py-refresh/test_refresh.py
+++ py-refresh/test_refresh.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 
-version = 4.0
+
+class RefreshClass(object):
+    def __init__(self):
+        self.value = 1
+
+    def print_info(self):
+        print('RefreshClass value: {} ver1.0'.format(self.value))
+
+version = 1.0
 
 print(version)
