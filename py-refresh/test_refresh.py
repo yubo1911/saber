acab4bda06a9076e588a6b4bbd36f343552728b7
yubo <yubo1911@163.com>
2016-12-13 00:08:26 -0800

    use signal to trigger refresh

--- py-refresh/test_refresh.py
+++ py-refresh/test_refresh.py
@@ -1,19 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 
-import os
 import time
+import signal
+
 import refresh_class
 
-
-version = 1.0
-
-print(version)
-
 rc = refresh_class.RefreshClass()
+
+
+def handl_refresh(signum, frame):
+    reload(refresh_class)
+    rc.__class__ = refresh_class.RefreshClass
+
+
+signal.signal(signal.SIGUSR1, handl_refresh)
 while True:
-    if os.path.exists('refresh.signal'):
-        reload(refresh_class)
-        rc.__class__ = refresh_class.RefreshClass
     time.sleep(5)
     rc.print_info()
