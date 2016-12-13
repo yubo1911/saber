304176285cc735e1c7079c15076ae345f919a288
yubo <yubo1911@163.com>
2016-12-12 22:28:30 -0800

    use a file to trigger the refresh action.

---
+++ py-refresh/refresh_class.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+
+class RefreshClass(object):
+    def __init__(self):
+        self.value = 1
+
+    def print_info(self):
+        print('RefreshClass value: {} ver2.0'.format(self.value))
--- py-refresh/test_refresh.py
+++ py-refresh/test_refresh.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 
+import os
+import time
+import refresh_class
 
-class RefreshClass(object):
-    def __init__(self):
-        self.value = 1
-
-    def print_info(self):
-        print('RefreshClass value: {} ver1.0'.format(self.value))
 
 version = 1.0
 
 print(version)
+
+rc = refresh_class.RefreshClass()
+while True:
+    if os.path.exists('refresh.signal'):
+        reload(refresh_class)
+        rc.__class__ = refresh_class.RefreshClass
+    time.sleep(5)
+    rc.print_info()
